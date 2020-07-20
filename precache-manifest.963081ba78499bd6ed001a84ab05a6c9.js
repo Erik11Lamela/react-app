b27ad225076b62b419338a39c60d6c37e9d28d15
Erik11Lamela <65742900+Erik11Lamela@users.noreply.github.com>
2020-07-20 16:00:07 +0500

    Updates

---
+++ precache-manifest.963081ba78499bd6ed001a84ab05a6c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e096df8081b92dc7eb6b0efc06dfa3",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "4e7847bdeabf1b3f6496",
+    "url": "/react-app/static/css/main.e876fa65.chunk.css"
+  },
+  {
+    "revision": "d335d67a5a634583644d",
+    "url": "/react-app/static/js/2.72fe3e23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.72fe3e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e7847bdeabf1b3f6496",
+    "url": "/react-app/static/js/main.4d99152a.chunk.js"
+  },
+  {
+    "revision": "1b3da175ff14529f861e",
+    "url": "/react-app/static/js/runtime-main.15ee4613.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
